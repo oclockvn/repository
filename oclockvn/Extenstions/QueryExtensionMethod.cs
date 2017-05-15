206e2e4881f220be618d0c0f1439784bcdf4962c
oclockvn <oclockvn@gmail.com>
2017-05-15 18:48:47 +0700

    rename folder query extenstion

--- oclockvn/Extenstions/QueryExtensionMethod.cs
+++ oclockvn/Extenstions/QueryExtensionMethod.cs
@@ -3,7 +3,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
 
-namespace oclockvn.QueryExtenstions
+namespace oclockvn.Extenstions
 {
     public static class QueryExtensionMethod
     {
--- oclockvn/Repository/Repository.cs
+++ oclockvn/Repository/Repository.cs
@@ -1,4 +1,4 @@
-﻿using oclockvn.QueryExtenstions;
+﻿using oclockvn.Extenstions;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
