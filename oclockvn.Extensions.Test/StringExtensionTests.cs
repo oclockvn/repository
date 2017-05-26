caddb095fd21a54f8795e721416039e75c650e17
oclockvn <oclockvn@gmail.com>
2017-05-26 15:05:51 +0700

    added string extension

---
+++ oclockvn.Extensions.Test/StringExtensionTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace oclockvn.Extensions.Test
+{
+    [TestFixture]
+    public class StringExtensionTests
+    {
+        [Test]
+        public void ShouldReturnCorrectFriendlyUrl()
+        {
+            var samsung = "sản phẩm samsung galaxy a7 2017";
+            var check = "Kiểm tra có hàng tại nơi bạn ở không?";
+            var mark = "(đường dẫn [có dấu ngoặc]  và nhiều khoảng trắng     )";
+            var symbol = "----có dấu gạch__ * khác với gạch ngang!@#$% aaa";
+
+            var samsungUrl = samsung.ToFriendlyUrl();
+            var checkUrl = check.ToFriendlyUrl();
+            var markUrl = mark.ToFriendlyUrl();
+            var symbolUrl = symbol.ToFriendlyUrl();
+
+            Assert.AreEqual("san-pham-samsung-galaxy-a7-2017", samsungUrl);
+            Assert.AreEqual("kiem-tra-co-hang-tai-noi-ban-o-khong", checkUrl);
+            Assert.AreEqual("duong-dan-co-dau-ngoac-va-nhieu-khoang-trang", markUrl);
+            Assert.AreEqual("co-dau-gach-khac-voi-gach-ngang-aaa", symbolUrl);
+        }
+    }
+}
---
+++ oclockvn.Extensions/StringExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace oclockvn.Extensions
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Cut given string for shorter string
+        /// </summary>
+        /// <param name="s">Given string to cut</param>
+        /// <param name="length">Max length to display</param>
+        /// <param name="ellipsis">The end string if over length</param>
+        /// <returns>The cutted string</returns>
+        public static string Truncate(this string s, int length = 50, string ellipsis = "...")
+        {
+            if (string.IsNullOrEmpty(s))
+                return string.Empty;
+
+            if (s.Length <= length)
+                return s;
+
+            return s.Substring(0, length).Trim() + ellipsis;
+        }
+
+        /// <summary>
+        /// Remove unicode character (unicode) of given string
+        /// </summary>
+        /// <param name="input">The input string to remove mark</param>
+        /// <returns>The string without mark</returns>
+        public static string RemoveMark(this string input)
+        {
+            var regex = new Regex("\\p{IsCombiningDiacriticalMarks}+");
+            var formD = input.Normalize(NormalizationForm.FormD);
+
+            return regex.Replace(formD, string.Empty)
+                .Replace('\u0111', 'd')
+                .Replace('\u0110', 'D');
+        }
+
+        /// <summary>
+        /// Convert a string into friendly url form with hiphen between words
+        /// </summary>
+        /// <param name="input">The input string</param>
+        /// <param name="length">The maximum length of result</param>
+        /// <returns>The string result</returns>
+        public static string ToFriendlyUrl(this string input, int length = 125)
+        {
+            if (string.IsNullOrEmpty(input))
+                return string.Empty;
+
+            string slug = input.RemoveMark().ToLower();
+
+            // remove invalid chars
+            slug = Regex.Replace(slug, @"[^a-z0-9\s-]", string.Empty);
+
+            // convert multiple spaces into one
+            slug = Regex.Replace(slug, "\\s+", " ").Trim();
+            slug = Regex.Replace(slug, "\\s", "-");
+            slug = Regex.Replace(slug, "-+", "-");
+            slug = slug.Trim('-');
+
+            // cut and trim            
+            return slug.Truncate(length, string.Empty);
+        }
+    }
+}
