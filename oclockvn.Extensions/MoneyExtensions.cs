7a02fe9fe34e528a4b8819b8b8373089e56c15fd
oclockvn <oclockvn@gmail.com>
2017-05-26 14:38:23 +0700

    add document comment

--- oclockvn.Extensions/MoneyExtensions.cs
+++ oclockvn.Extensions/MoneyExtensions.cs
@@ -4,6 +4,15 @@ namespace oclockvn.Extensions
 {
     public static class MoneyExtensions
     {
+        /// <summary>
+        /// Round a decimal into integer
+        /// </summary>
+        /// <param name="money">The input value</param>
+        /// <param name="unit">The unit of a money system. This is the minimum value in a money system.</param>
+        /// <param name="min">
+        /// The minimum value to compare. Default it is equal to unit
+        /// </param>
+        /// <returns>The rounded money</returns>
         public static int ToRoundMoney(this decimal money, int unit = 0, int min = 0)
         {
             if (money == 0)
@@ -23,6 +32,11 @@ namespace oclockvn.Extensions
             return negative ? -val : val;
         }
 
+        /// <summary>
+        /// Round money in Vietnam money system
+        /// </summary>
+        /// <param name="money">The input value</param>
+        /// <returns>The rounded money</returns>
         public static int ToVnd(this decimal money)
         {
             return money.ToRoundMoney(500, 200);
