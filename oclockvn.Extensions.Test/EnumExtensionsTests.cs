e185ffb72cf67e07207d8cedfb867a6f492749a3
oclockvn <oclockvn@gmail.com>
2017-05-26 15:11:57 +0700

    added enum extensions

---
+++ oclockvn.Extensions.Test/EnumExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace oclockvn.Extensions.Test
+{
+    [TestFixture]
+    public class EnumExtensionsTests
+    {
+        internal enum Tests
+        {
+            [Display(Name = "First value")]
+            FirstValue,
+            SecondValue
+        }
+
+        [Test]
+        public void ShouldReturnCorrentDisplayName()
+        {
+            var first = Tests.FirstValue;
+            var second = Tests.SecondValue;
+
+            Assert.AreEqual("First value", first.ToDisplayName());
+            Assert.AreEqual("SecondValue", second.ToDisplayName());
+        }
+    }
+}
---
+++ oclockvn.Extensions/EnumExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace oclockvn.Extensions
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Get name of display attribute of an enum value
+        /// </summary>
+        /// <param name="enumValue">The enum value to get</param>
+        /// <returns>The name property of display attribute of exist, otherwise return enum to string</returns>
+        public static string ToDisplayName(this Enum enumValue)
+        {
+            var attr = enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .First()
+                            .GetCustomAttribute<DisplayAttribute>();
+
+            return attr != null ? attr.GetName() : enumValue.ToString();
+        }
+    }
+}
