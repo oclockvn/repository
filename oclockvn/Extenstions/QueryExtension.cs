736c626fa49e97af08000eead5e173dc0b65914b
oclockvn <oclockvn@gmail.com>
2018-09-30 23:20:23 +0700

    added paging extension

--- oclockvn/Extenstions/QueryExtension.cs
+++ oclockvn/Extenstions/QueryExtension.cs
@@ -18,5 +18,23 @@ namespace oclockvn.Extenstions
         {
             return includeProperties.Aggregate(self, (current, includeProperty) => current.Include(includeProperty));
         }
+
+        /// <summary>
+        /// Pagings the specified current query.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="query">The query.</param>
+        /// <param name="current">The current.</param>
+        /// <param name="take">The take.</param>
+        /// <returns>The IQueryable after paging</returns>
+        public static IQueryable<T> Paging<T>(this IQueryable<T> query, int current, int take) where T : class
+        {
+            if (current < 1)
+                current = 1;
+
+            var skip = (current - 1) * take;
+
+            return query.Skip(skip).Take(take);
+        }
     }
 }
